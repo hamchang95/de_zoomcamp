1c8d11fda5c3ba44ae856f1798b7debc9252ba90
hamchang95 <46330993+hamchang95@users.noreply.github.com>
2026-01-17 11:27:02 +0000

    cli update

--- ingest_data.py
+++ ingest_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import click
 
 dtype = {
     "VendorID": "Int64",
@@ -26,20 +27,18 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-def run():
-    # define parameters for postgresdb & ingestion
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
-    
-    year = 2021
-    month = 1
-
-    target_table = 'yellow_taxi_data'
-    
-    chunksize = 100000
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of data to ingest')
+@click.option('--month', default=1, type=int, help='Month of data to ingest')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
+    """Ingest NYC taxi data into PostgreSQL database."""
     
     # data to ingest
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
@@ -73,10 +72,10 @@ def run():
             
         # iterate append   
         df_chunk.to_sql(
-        name=target_table,
-        con=engine,
-        if_exists="append"
-    )
+            name=target_table,
+            con=engine,
+            if_exists="append"
+        )
 
 if __name__ == '__main__':
     run()
